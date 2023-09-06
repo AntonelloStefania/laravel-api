772a3b6153c3466d7c340bdbb97b0fcb5da5f096
AntonelloStefania <antonelloste94@gmail.com>
2023-09-06 15:48:57 +0200

    create pagine con rotte

--- app/Http/Controllers/Api/SiteController.php
+++ app/Http/Controllers/Api/SiteController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PortfolioModel;
+//use App\Models\Types;
+//use App\Models\Technology;
 
 class SiteController extends Controller
 {
